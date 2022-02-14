c1ac76bd6c04ed0694d30c6cae57b45db8496431
maxmarsc <maxime.coutant@grenoble-inp.org>
2022-02-14 15:56:13 +0100

    Cleaned up a bit and documented the new functions

--- main/ilog_thread.h
+++ main/ilog_thread.h
@@ -1,9 +1,26 @@
 #ifndef __ILOG_THREAD_H__
 #define __ILOG_THREAD_H__
 
+/**
+ * @brief Log the given message, lock-free and thread safe
+ * 
+ * @param msg The message to log, will be sent asynchronously by the ILOG thread
+ */
+void log_msg(char* msg);
+
+
+/**
+ * @brief Start the ILOG thread then return
+ */
 void init_ilog_thread();
+
+/**
+ * @brief Signal the ILOG thread it should terminate, then wait for it
+ * 
+ * @note The ILOG thread will wait for the msg queue to be empty before
+ * terminating, this way we make sure we don't miss some function call
+ */
 void join_ilog_thread();
 
-void log_msg(char* msg);
 
 #endif //__ILOG_THREAD_H__
