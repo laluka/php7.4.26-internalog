bb63c48be974ec1839a8fc6a3d075a5926b635c5
TheLaluka <loukajc@gmail.com>
2022-08-11 00:33:09 +0200

    added ignored functoins

--- log-ingester/udp-ingester.py
+++ log-ingester/udp-ingester.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import socket
+from rich import print as pprint
+import json
+
+ignored_fcts = []
+ignored_fcts.append("preg_replace")
 
 def udp_server(host='127.0.0.1', port=5555):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,4 +19,12 @@ def udp_server(host='127.0.0.1', port=5555):
 
 
 for data in udp_server():
-    print(f"> {data}")
+    if "sulf" in data.lower():
+        log_blob = json.loads(data)
+        should_print = True
+        for ignored_fct in ignored_fcts:
+            if ignored_fct == log_blob.get("name"):
+                should_print = False
+                continue
+        if should_print:
+            pprint(log_blob)
